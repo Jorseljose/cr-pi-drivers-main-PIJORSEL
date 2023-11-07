f4ffb1aa800bbb1e088b2afce3cdfd02a35cee7d
Jorseljose <Jorselmaurera@gmail.com>
2023-11-07 19:17:31 -0300

    otro

--- client/src/Components/Select-Multiples/SelectMultiples.jsx
+++ client/src/Components/Select-Multiples/SelectMultiples.jsx
@@ -1,18 +1,13 @@
 import { useState } from "react";
 import styles from "../Select-Multiples/SelectMultiples.module.css";
 
-export default function SelectMultiples({ placeholder, onChange, label }) {
-  const [values, setValues] = useState([]);
+const options = {};
 
+export default function SelectMultiples({ label }) {
   return (
     <div className={styles.Select}>
       <div className={styles.label}>{label}</div>
-      <div className={styles.container}>
-        {values.map((value) => (
-          <div key={value}>{value}</div>
-        ))}
-        <input className={styles.input} placeholder={placeholder} />
-      </div>
+      <div className={styles.container}></div>
     </div>
   );
 }
